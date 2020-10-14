525902f8647c9ced314dd96412a8036431b5ce7d
Daniel Varga <daxendor13@gmail.com>
2020-10-14 08:41:29 +0200

    auxiliary scripts are ready and mobile version is complete

--- scripts/pontetest.js
+++ scripts/pontetest.js
@@ -1,3 +1,37 @@
+//Dropdown Menu open/close
+$('#dropdownMenu').click(function() {
+    if ($(this).hasClass('active')) {
+        //slide dropdown menu out of viewport
+        $(this).removeClass('active');
+        $('#dropdownContainer').animate({
+            right: '-=100vw'
+        }, 100);
+        //close opened sub-options
+        $('.option').each(function() {
+            $('.option').removeClass('visible');
+        });
+    } else {
+        //slide dropdown menu into viewport
+        $(this).addClass('active')
+        $('#dropdownContainer').animate({
+            right: '+=100vw'
+        }, 100);
+    }
+});
+//Dropdown Menu sub-option open/close
+$('.option').click(function() {
+    let dropdownNum = $(this).attr('data-drpdwn-num');
+    let dropdownSubArray = $('.option[data-drpdwn-sub-num=' + dropdownNum + ']');
+    
+    dropdownSubArray.each(function() {
+        if ($(this).hasClass('visible')) {
+            $(this).removeClass('visible');
+        } else {
+            $(this).addClass('visible');
+        }
+    });
+});
+
 //Raise corresponding label if input is empty
 $('.form-input').focusin(function () {
     if (this.value == '') {
@@ -23,4 +57,5 @@ $('#message').on('keyup keydown', (function() {
     let charCount = this.value.length;
     
     charCounter.text(200 - charCount);
-}))
+}));
+
